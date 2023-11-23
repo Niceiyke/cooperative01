9e6314f98171e6550bd07e402ba029a53e38ea86
niceiyke <niceiyke04@gmail.com>
2023-11-23 06:09:30 +0100

    update

--- frontend/ampcus/app/login/page.jsx
+++ frontend/ampcus/app/login/page.jsx
@@ -38,10 +38,10 @@ const Login = () => {
 
         try {
             const response = await fetch('http://127.0.0.1:8000/api/login/', {
+
                 method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
+                headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
                 body: JSON.stringify(formData),
             });
 
