730a756ef5fbbe095959a16f9278c5bbb6b5af59
Niceiyke <74918186+Niceiyke@users.noreply.github.com>
2023-11-29 17:45:43 +0100

    update

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -27,9 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -87,13 +88,29 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+if DEBUG:
+     DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
+
+  
+else:
+       DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'verceldb',
+        'USER': 'default',
+        'PASSWORD': "kmQ0SCHcAX7O",
+        'HOST':"ep-super-frog-53680855-pooler.us-east-1.postgres.vercel-storage.com",
+        'PORT': '5432',
+    }
+    }
+
+   
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
