de9f742af613a90c48ce3675ec58b0cd8361febb
niceiyke <niceiyke04@gmail.com>
2023-12-02 04:31:54 +0100

    update

--- backend/core/accounts/signals.py
+++ backend/core/accounts/signals.py
@@ -3,14 +3,14 @@ from django.dispatch import receiver
 from members.models import Member
 from .models import CustomUser
 from django.core.mail import send_mail
+import pyshorteners
 
 
 @receiver(post_save, sender=CustomUser)
 def create_member_profile(sender, instance, created, **kwargs):
     if created:
-        member=Member.objects.create(user=instance)
+        member = Member.objects.create(user=instance)
         member.save()
-
 
 
 @receiver(post_save, sender=CustomUser)
@@ -20,8 +20,14 @@ def save_member_profile(sender, instance, **kwargs):
 
 @receiver(post_save, sender=CustomUser)
 def send_successefull_signup(sender, instance, created, **kwargs):
+    url = f"http://127.0.0.1:8000/api/member-update/{instance.member.id} "
+    s = pyshorteners.Shortener()
+
+    short_url = s.tinyurl.short(url)
+    print("Shortened URL:", short_url)
+
     subject = "Successfull Registration "
-    message = f"Thank you for registering with Ama Cooperative Society.\nuse this link http://127.0.0.1:8000/api/member-update/{instance.member.id} to update your profile "
+    message = f"Thank you for registering with Ama Cooperative Society.\nuse this link {short_url} to update your profile "
     from_email = "Info@ampcus.com"
     recipient_list = [instance.email]
 
